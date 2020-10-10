8c60c940645dee97b5edcc745ee15d4282af31f9
KyklyMachine <71447642+KyklyMachine@users.noreply.github.com>
2020-10-10 21:00:31 +0300

    Android

---
+++ Android Prog/app/src/main/java/com/example/lastp/ModelClasses.java
@@ -0,0 +1,34 @@
+package com.example.lastp;
+
+public class ModelClasses {
+    public static String[] MODEL_CLASSES = new String[]{
+            "½ kopek 1925 - 1928",
+            "1 chervonets 1923",
+            "1 kopek 1924 - 1991",
+            "1 poltinnik 1924 - 1927",
+            "1 ruble 1921 - 1922",
+            "1 ruble 1924",
+            "1 ruble 1958 - 1991",
+            "2 kopeks 1924 - 1991",
+            "3 kopeks 1924 - 1991",
+            "5 kopeks 1924 - 1991",
+            "5 rubles 1991",
+            "10 kopeks 1921 - 1923",
+            "10 kopeks 1924 - 1931, 1958 - 1991",
+            "10 kopeks 1931 - 1934",
+            "10 kopeks 1935 - 1957",
+            "10 rubles 1991 - 1992",
+            "15 kopeks 1921 - 1923",
+            "15 kopeks 1924 - 1931, 1958 - 1991",
+            "15 kopeks 1931 - 1934",
+            "15 kopeks 1935 - 1957",
+            "20 kopeks 1921 - 1923",
+            "20 kopeks 1924 - 1931, 1958 - 1991",
+            "20 kopeks 1931 - 1934",
+            "20 kopeks 1935 - 1957",
+            "50 kopeks 1921 - 1922",
+            "50 kopeks 1923 - 1991"
+
+    };
+}
+
